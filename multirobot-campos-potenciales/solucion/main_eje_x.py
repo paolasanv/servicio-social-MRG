e0a2816f4fc4217be6789a33205a3074db9a1b80
sanv.leslie@ciencias.unam.mx <sanv.leslie@ciencias.unam.mx>
2026-09-07 15:28:23 -0600

    7/agosto

--- multirobot-campos-potenciales/solucion/main_eje_x.py
+++ multirobot-campos-potenciales/solucion/main_eje_x.py
@@ -9,22 +9,25 @@ from src.controlador import Controlador
 
 # ----- protegido -----
 # Robot von ArUco 1 (protegido)
-protegido = Robot("192.168.0.101")  #microusb
-PROTEGIDO_VEL_LIN = 0.5
-TIEMPO_MOVIMIENTO_PROTEGIDO = 10.0  # segundos
+protegido = Robot("192.168.0.101")  # caiman verde y blanco
+
+PROTEGIDO_VEL_LIN = 0.2
+TIEMPO_ESPERA_PROTEGIDO = 3.0      # segundos antes de comenzar
+TIEMPO_MOVIMIENTO_PROTEGIDO = 10.0  # segundos que permanece avanzando
 # ----------------------
 
-#------- atacante -----
+# ----- atacante -----
 # Robot con ArUco 2 (atacante)
-atacante = Robot("192.168.0.100") #usb-c (caimanes amarillos)
-#-----------------------
+atacante = Robot("192.168.0.100")  # caimanes amarillos
+# ---------------------
 
 vision = Vision()
 controlador = Controlador()
 
-# AruCo 0 = es la referencia
-# ArUco 1 = es el robot protegido 
-# ArUco 2 = es el robot atacante
+# ArUco 0 = referencia
+# ArUco 1 = robot protegido
+# ArUco 2 = robot atacante
+
 
 def ejecutar():
     ultimo_envio_protegido = 0
@@ -35,91 +38,59 @@ def ejecutar():
 
         vision.iniciar_camara()
         inicio = time.time()
-
+        
         while True:
+
             if tecla == ord('q'):
                 break
 
             ahora = time.time()
+            tiempo_transcurrido = ahora - inicio
 
-            # ==================================================
+            # ==============================================
+            # ESTADO DEL ROBOT PROTEGIDO
+            # ==============================================
+            protegido_avanzar = (tiempo_transcurrido >= TIEMPO_ESPERA_PROTEGIDO and tiempo_transcurrido < ( TIEMPO_ESPERA_PROTEGIDO + TIEMPO_MOVIMIENTO_PROTEGIDO))
+
+            # ==============================================
             # 1) CONTROL DEL ROBOT PROTEGIDO
-            # ==================================================
-            
-            vr_protegido, vl_protegido = protegido.calcular_velocidades_ruedas(PROTEGIDO_VEL_LIN, 0)
-            if ahora - inicio < TIEMPO_MOVIMIENTO_PROTEGIDO:
-                vr_protegido, vl_protegido = protegido.calcular_velocidades_ruedas(PROTEGIDO_VEL_LIN, 0)
-            else:
-                vr_protegido, vl_protegido = 0, 0
+            # ==============================================
 
-            if ahora - ultimo_envio_protegido >= SEND_PERIOD:
-                protegido.enviar_velocidades(vl_protegido, vr_protegido)
-                ultimo_envio_protegido = ahora
-        
-            # ==================================================
-            # 2) CONTROL DEL ROBOT ATACANTE
-            # ==================================================
-            
+            if protegido_avanzar:
+                vr_protegido, vl_protegido = (protegido.calcular_velocidades_ruedas(PROTEGIDO_VEL_LIN,0))
+
+                if ahora - ultimo_envio_protegido >= SEND_PERIOD:
+                    protegido.enviar_velocidades(vl_protegido, vr_protegido)
+                    ultimo_envio_protegido = ahora
+
+            else:
+                protegido.detener()
+
             frame, poses, tecla = vision.obtener_poses()
-            if (1 in poses and 2 in poses):
+
+            # ==============================================
+            # 3) CONTROL DEL ROBOT ATACANTE
+            # ==============================================
+
+            if protegido_avanzar and (1 in poses and 2 in poses):
                 robot_atacante_pose = poses[2]
                 robot_protegido_pose = poses[1]
 
-                # ==================================================
-                # DEBUG: POSICIONES Y ORIENTACIONES
-                # ==================================================
-                """
-                dx = robot_protegido_pose["x"] - robot_atacante_pose["x"]
-                dy = robot_protegido_pose["y"] - robot_atacante_pose["y"]
-
-                theta_r = robot_atacante_pose["theta"]
-
-                theta_g = np.arctan2(dy, dx)
-
-                theta_e = np.arctan2(
-                    np.sin(theta_g - theta_r),
-                    np.cos(theta_g - theta_r))
-
-                print(
-                    f"\n"
-                    f"Atacante: "
-                    f"x={robot_atacante_pose['x']:.3f}, "
-                    f"y={robot_atacante_pose['y']:.3f}, "
-                    f"theta={np.degrees(theta_r):.1f}°\n"
-                    f"Objetivo:  "
-                    f"x={robot_protegido_pose['x']:.3f}, "
-                    f"y={robot_protegido_pose['y']:.3f}\n"
-                    f"dx={dx:.3f}, "
-                    f"dy={dy:.3f}\n"
-                    f"theta_g={np.degrees(theta_g):.1f}°, "
-                    f"theta_e={np.degrees(theta_e):.1f}°")
-                """
-
-                # ==================================================
-
-              
-                v_atacante, w_atacante = controlador.control_potencial(
-                    robot_atacante_pose["x"],
-                    robot_atacante_pose["y"],
-                    robot_atacante_pose["theta"],
-                    robot_protegido_pose["x"],
-                    robot_protegido_pose["y"])
+                v_atacante, w_atacante = (controlador.control_potencial_atrac_rep(
+                        robot_atacante_pose["x"],
+                        robot_atacante_pose["y"],
+                        robot_atacante_pose["theta"],
+                        robot_protegido_pose["x"],
+                        robot_protegido_pose["y"]))
+                
                 vr_atacante, vl_atacante = (atacante.calcular_velocidades_ruedas(v_atacante, w_atacante))
-                """
-                print(
-                    f"Control: "
-                    f"v={v_atacante:.3f} m/s, "
-                    f"w={w_atacante:.3f} rad/s | "
-                    f"vr={vr_atacante:.3f} rad/s, "
-                    f"vl={vl_atacante:.3f} rad/s")
-                """
+
                 if ahora - ultimo_envio_atacante >= SEND_PERIOD:
                     atacante.enviar_velocidades(vr_atacante, vl_atacante)
                     ultimo_envio_atacante = ahora
-                    
+
             else:
-                # Si no hay visión del robot seguidor, detenerlo por seguridad
-                atacante.detener()   
+                atacante.detener()
 
     except KeyboardInterrupt:
         print("\nInterrupción por teclado.")
@@ -128,12 +99,9 @@ def ejecutar():
         print("Deteniendo robots...")
         protegido.detener()
         protegido.cerrar()
-        
         atacante.detener()
         atacante.cerrar()
-
         vision.cerrar()
-
 
 if __name__ == "__main__":
     ejecutar()
--- multirobot-campos-potenciales/solucion/src/controlador.py
+++ multirobot-campos-potenciales/solucion/src/controlador.py
@@ -10,7 +10,10 @@ class Controlador:
         self.v_max = 3
         self.w_max = 1
         self.k_r = 0.15
-        self.dist_seguridad = 0.45
+        self.dist_seguridad = 0.5
+        
+        self.dist_repulsion = 0.6
+        self.k_rep = 0.2
 
         # Movimiento en eje x
         self.direccion = 1
@@ -27,8 +30,7 @@ class Controlador:
         d = np.hypot(dx, dy)
 
         theta_g = np.arctan2(dy, dx)
-        theta_e = np.arctan2(np.sin(theta_g - theta_r),
-                             np.cos(theta_g - theta_r))
+        theta_e = np.arctan2(np.sin(theta_g - theta_r),np.cos(theta_g - theta_r))
 
         error = max(0.0, d - self.dist_seguridad)
 
@@ -43,3 +45,71 @@ class Controlador:
         w = np.clip(w, -self.w_max, self.w_max)
 
         return v, w
+
+    def control_potencial_atrac_rep(self, xr, yr, theta_r, xo, yo):
+
+        dx = xo - xr
+        dy = yo - yr
+
+        d = np.hypot(dx, dy)
+
+        # ==========================================
+        # DIRECCIÓN HACIA EL PROTEGIDO
+        # ==========================================
+
+        theta_g = np.arctan2(dy, dx)
+
+        theta_e = np.arctan2(np.sin(theta_g - theta_r), np.cos(theta_g - theta_r))
+
+        # ==========================================
+        # CAMPO ATRACTIVO
+        # ==========================================
+        error_atractivo = max(0.0, d - self.dist_seguridad)
+
+        v_atractiva = self.kv * min(error_atractivo, self.k_r)
+        w_atractiva = -self.kw * np.sin(theta_e)
+
+        # ==========================================
+        # CAMPO REPULSIVO
+        # ==========================================
+
+        if d < self.dist_seguridad and d > 0:
+
+            theta_repulsiva = np.arctan2(yr - yo, xr - xo)
+            theta_rep_error = np.arctan2( np.sin(theta_repulsiva - theta_r), np.cos(theta_repulsiva - theta_r))
+
+            # --------------------------------------
+            # Fuerza repulsiva
+            # --------------------------------------
+
+            fuerza_repulsiva = self.k_rep * (self.dist_seguridad - d)
+            v_repulsiva = fuerza_repulsiva
+            w_repulsiva = self.kw * np.sin(theta_rep_error)
+
+        else:
+
+            v_repulsiva = 0.0
+            w_repulsiva = 0.0
+
+        # ==========================================
+        # COMBINACIÓN DE CAMPOS
+        # ==========================================
+
+        if d >= self.dist_seguridad:
+
+            v = v_atractiva
+            w = w_atractiva
+
+        else:
+            v = -v_repulsiva
+            w = w_repulsiva
+
+        # ==========================================
+        # LIMITES
+        # ==========================================
+
+        v = np.clip(v,-self.v_max, self.v_max)
+        w = np.clip(w,-self.w_max,self.w_max)
+
+        return v, w
+
